209239c59fbd042e522b9924e7bbae485b33337c
FranzLee <142087004+FranzLee@users.noreply.github.com>
2024-06-13 14:50:48 +0800

    Add files via upload

---
+++ flaskForGomoku/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from engineio.async_drivers import threading
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret!"
+socketio = SocketIO(
+    app,
+    async_mode="threading"
+)
+
+allConnection = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@socketio.on('connect')
+def connect(data):
+    print("connect")
+    print("id : ", socketio.id)
+    # if(len(allConnection) == 1):
+    #     emit("setting", "black")
+    # else :
+    #     emit("setting", "white")
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socketio.on('newLoc')
+def newLoc(data):
+    print("receive newLoc")
+    
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+if(__name__ == "__main__") :
+    socketio.run(app, allow_unsafe_werkzeug=True)
+
+def ack():
+    print("message was received!")
